0c17c34113604a50509f1302fa45df757fe36646
PascalUlor <pascalulor@yahoo.com>
2019-05-07 14:40:15 +0100

    ft(implement callback to check if item is in an array)

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -67,7 +67,14 @@ multiplyNums(5, 8, callback);
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    cb(true)
+  } else {
+    cb(false)
+  }
 }
+// Function Invocation
+contains('Gum', items, callback);
 
 /* STRETCH PROBLEM */
 
