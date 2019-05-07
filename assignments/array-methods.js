4baa1ce2f40651936dedbdcdaee1495d9b5e7c14
PascalUlor <pascalulor@yahoo.com>
2019-05-07 15:27:33 +0100

    ft(get sum of all donations made)

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -79,11 +79,20 @@ console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+runners.filter(runner => {
+    if(runner.shirt_size === "L"){
+        largeShirts.push(runner)
+    }
+})
 console.log(largeShirts);
+console.table(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+let donations = runners.map(runner => runner.donation);
+let totalDonations = donations.reduce((a, b) => a + b)
+ticketPriceTotal.push(totalDonations);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
